d16bd2e2446700b0b9c16879ca3c10b2e95c5f1c
Kyle <kdorcey5103@gmail.com>
2019-02-06 19:05:32 -0600

    Created very bare-bones Homepage-user migration.

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    name = models.CharField(max_length = 1000)
+    password = models.CharField(max_length = 1000) #edit this later to make it encryptable
+    email = models.CharField(max_length=1000)
+    phone = models.IntegerField()
+    title = models.CharField(max_length = 100)
+    uuid = models.IntegerField()
+
+
+#class set_password(self, raw_password):
