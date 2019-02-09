da22ed14909b81e64ca605bde2d86b7395e486eb
GregMalicki <gregory-malicki@uiowa.edu>
2019-02-08 22:33:09 -0600

    added structure for authentication

--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from homepage.forms import LoginForm
 from django.http import HttpResponse
-
+from django.contrib.auth import authenticate
 # Create your views here.
 
 
@@ -18,5 +18,10 @@ def login(request):
     if request.method=='POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            cd = form.cleaned_data['username']
-            return HttpResponse(("<h1> {} </h1>").format(cd))
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                return HttpResponse('<h1> auth </h1>')
+            else:
+                return HttpResponse('<h1> bad </h1>')
+                # return render(request, 'login.html', {'form': LoginForm()})
