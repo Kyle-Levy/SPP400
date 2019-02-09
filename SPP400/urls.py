15527d4a546dc87e4ebd22c504f30cfc4931762e
GregMalicki <gregory-malicki@uiowa.edu>
2019-02-08 22:19:01 -0600

    added login screen to test page, with routing for login function

--- SPP400/urls.py
+++ SPP400/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from homepage.views import login
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', include('homepage.urls')),
+    path('login/', login),
+
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
-
+from homepage.forms import LoginForm
 from django.http import HttpResponse
 
 # Create your views here.
+
+
+
 def home(request):
-    return render(request, 'login.html')
+    form = LoginForm()
+    return render(request, 'login.html', {'form': form})
     #  return HttpResponse('<h1> Homepage </h1>')
 
 def about(request):
     return HttpResponse('<h1> Testerooni </h1>')
+
+def login(request):
+    if request.method=='POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data['username']
+            return HttpResponse(("<h1> {} </h1>").format(cd))
