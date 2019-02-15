2175704d0016c7e09b099040cbda21c12ee6b913
kylelevy <kyle.robert.levy@gmail.com>
2019-02-15 13:36:13 -0600

    Login data is being posted, still need to figure out how to use an assert to test login

--- homepage/tests_views.py
+++ homepage/tests_views.py
@@ -1,23 +1,27 @@
 from django.test import RequestFactory, TestCase
 from django.contrib.auth.models import User
+from django.contrib.sessions.middleware import SessionMiddleware
 
 from .views import log_in
+
 
 # Create your tests here.
 class TestLogin(TestCase):
 
-    #create setUp so every test can have access to the requestFactory
+    # create setUp so every test can have access to the requestFactory
+    # Needed to add session to the request: https://medium.com/@harshvb7/how-to-add-session-and-messages-in-django-requestfactory-16935a3351d0
     def setUp(self):
         self.factory = RequestFactory()
-
-
-    def test_login_page(self):
-        user = User.objects.create_user('testuser', 'testuser@email.com', 'super_secret_pass')
+        self.middleware = SessionMiddleware()
+        user = User.objects.create_superuser('testuser', 'testuser@email.com', 'super_secret_pass')
         user.save()
 
-        request = self.factory.post('login/', {'username': 'testuser', 'password': 'super_secret_pass'})
+    def test_login_page(self):
+        request = self.factory.post('login/', {'username': 'testuse', 'password': 'super_secret_pass'})
+
+        self.middleware.process_request(request)
+        request.session.save()
+
         response = log_in(request)
+
         print(response)
-
-
-
