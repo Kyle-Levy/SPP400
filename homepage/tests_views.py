9d821d5cbc2611a10643d5e7a1fcd01be7e4d7bc
kylelevy <kyle.robert.levy@gmail.com>
2019-02-15 12:33:59 -0600

    Updates to test files

--- homepage/tests_views.py
+++ homepage/tests_views.py
@@ -1,4 +1,23 @@
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
+from django.contrib.auth.models import User
+
+from .views import log_in
 
 # Create your tests here.
-#class UserTest(TestCase):
+class TestLogin(TestCase):
+
+    #create setUp so every test can have access to the requestFactory
+    def setUp(self):
+        self.factory = RequestFactory()
+
+
+    def test_login_page(self):
+        user = User.objects.create_user('testuser', 'testuser@email.com', 'super_secret_pass')
+        user.save()
+
+        request = self.factory.post('login/', {'username': 'testuser', 'password': 'super_secret_pass'})
+        response = log_in(request)
+        print(response)
+
+
+
