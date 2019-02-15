25172ffc04d2a89e9f032ac861a572358cce6f1c
GregMalicki <gregory-malicki@uiowa.edu>
2019-02-14 19:31:13 -0600

    added profile model to extend user, some diy 2 factor framework

--- homepage/models.py
+++ homepage/models.py
@@ -1,13 +1,35 @@
 from django.db import models
-
-# Create your models here.
-class Users(models.Model):
-    name = models.CharField(max_length = 1000)
-    password = models.CharField(max_length = 1000) #edit this later to make it encryptable
-    email = models.CharField(max_length=1000)
-    phone = models.IntegerField()
-    title = models.CharField(max_length = 100)
-    uuid = models.IntegerField()
+from django.contrib.auth.models import User
+from datetime import datetime
+import random
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
-#class set_password(self, raw_password):
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    key = models.IntegerField(default=1)
+    key_expiration = models.DateTimeField(default=datetime.now)
+
+    def new_key(self):
+        self.key = random.randint(1000, 9999)
+
+    def check_key(self, num):
+        if self.key == num:
+            # self.new_key()
+            return True
+        else:
+            return False
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        profile = Profile.objects.create(user=instance)
+        profile.save()
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
