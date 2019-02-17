4176c68a4d9ba732c5200cdaca118608eba06806
kylelevy <kyle.robert.levy@gmail.com>
2019-02-17 11:47:56 -0600

    Fixed tests that had errors due to the addition of the workflow app

--- homepage/tests_views.py
+++ homepage/tests_views.py
@@ -10,15 +10,25 @@ class TestLogin(TestCase):
 
     # create setUp so every test can have access to the requestFactory
     # Needed to add session to the request: https://medium.com/@harshvb7/how-to-add-session-and-messages-in-django-requestfactory-16935a3351d0
+    '''
+    Tests were failing with the error:
+    ValueError: modules context processor requires "django.contrib.auth.context_processors.auth"
+    to be in TEMPLATE_CONTEXT_PROCESSORS in your settings file.
+
+    This person had a similar issue with their fix being done below:
+    https://stackoverflow.com/questions/50513310/django-test-will-not-notice-django-contrib-auth-context-processors-auth/50514240#50514240
+    '''
+
+    #A request must have a user tied to it, but a login can still be unsuccessful if the data posted is invalid
     def setUp(self):
         self.factory = RequestFactory()
         self.middleware = SessionMiddleware()
-        user = User.objects.create_superuser('testuser', 'testuser@email.com', 'super_secret_pass')
-        user.save()
+        self.user = User.objects.create_superuser('testuser', 'testuser@email.com', 'super_secret_pass')
+        self.user.save()
 
     def test_login_page_good_credentials(self):
         request = self.factory.post('login/', {'username': 'testuser', 'password': 'super_secret_pass'})
-
+        request.user = self.user
         self.middleware.process_request(request)
         request.session.save()
 
@@ -28,7 +38,7 @@ class TestLogin(TestCase):
 
     def test_login_page_wrong_password(self):
         request = self.factory.post('login/', {'username': 'testuser', 'password': 'soup_secret_pass'})
-
+        request.user = self.user
         self.middleware.process_request(request)
         request.session.save()
 
@@ -38,7 +48,7 @@ class TestLogin(TestCase):
 
     def test_login_page_wrong_username(self):
         request = self.factory.post('login/', {'username': '418', 'password': 'super_secret_pass'})
-
+        request.user = self.user
         self.middleware.process_request(request)
         request.session.save()
 
