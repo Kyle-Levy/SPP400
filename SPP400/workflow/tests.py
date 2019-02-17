b9e5bbfdd67ae1e533ac6daf89c4fe15a53b0515
Evan Meyer <evanmeyer07@gmail.com>
2019-02-16 18:26:04 -0600

    Adding tests for workflow. Project code coverage is at 86%. Command: coverage report --omit*/.local/*

--- SPP400/workflow/tests.py
+++ SPP400/workflow/tests.py
@@ -1,3 +1,61 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from viewflow.models import Process
 
-# Create your tests here.
+
+class Test(TestCase):
+    def setUp(self):
+        User.objects.create_superuser('admin', 'admin@example.com', 'password')
+        self.client.login(username='admin', password='password')
+
+    def testApproved(self):
+        self.client.post(
+            '/workflow/workflow/workflow/start/',
+            {
+                'text': 'urine test',
+                '_viewflow_activation-started': '2019-01-01'
+            }
+        )
+
+        self.client.post(
+            '/workflow/workflow/workflow/1/approve/2/assign/'
+        )
+
+        self.client.post(
+            'workflow/workflow/workflow/1/approve/2/',
+            {
+                'approved': True,
+                '_viewflow_activation-started': '2019-01-01'
+            }
+        )
+
+        process = Process.objects.get()
+
+        self.assertEquals('NEW', process.status)
+        self.assertEquals(2, process.task_set.count())
+
+    def testNotApproved(self):
+        self.client.post(
+            '/workflow/workflow/workflow/start/',
+            {
+                'text': 'urine test',
+                '_viewflow_activation-started': '2019-01-01'
+            }
+        )
+
+        self.client.post(
+            '/workflow/workflow/workflow/1/approve/2/assign/'
+        )
+
+        self.client.post(
+            'workflow/workflow/workflow/1/approve/2/',
+            {
+                'approved': False,
+                '_viewflow_activation-started': '2019-01-01'
+            }
+        )
+
+        process = Process.objects.get()
+
+        self.assertEquals('NEW', process.status)
+        self.assertEquals(2, process.task_set.count())
