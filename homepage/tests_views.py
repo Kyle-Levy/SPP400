9fa9dcd0581c4eb416353cdb458f34276bb06238
kylelevy <kyle.robert.levy@gmail.com>
2019-02-17 12:34:10 -0600

    Added logout test

--- homepage/tests_views.py
+++ homepage/tests_views.py
@@ -2,12 +2,11 @@ from django.test import RequestFactory, TestCase
 from django.contrib.auth.models import User
 from django.contrib.sessions.middleware import SessionMiddleware
 
-from .views import log_in
+from .views import log_in, log_out
 
 
 # Create your tests here.
 class TestLogin(TestCase):
-
     # create setUp so every test can have access to the requestFactory
     # Needed to add session to the request: https://medium.com/@harshvb7/how-to-add-session-and-messages-in-django-requestfactory-16935a3351d0
     '''
@@ -19,7 +18,7 @@ class TestLogin(TestCase):
     https://stackoverflow.com/questions/50513310/django-test-will-not-notice-django-contrib-auth-context-processors-auth/50514240#50514240
     '''
 
-    #A request must have a user tied to it, but a login can still be unsuccessful if the data posted is invalid
+    # A request must have a user tied to it, but a login can still be unsuccessful if the data posted is invalid
     def setUp(self):
         self.factory = RequestFactory()
         self.middleware = SessionMiddleware()
@@ -75,3 +74,25 @@ class TestLogin(TestCase):
         response = log_in(request)
 
         self.assertIsNone(response)
+
+
+class TestLogout(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.middleware = SessionMiddleware()
+        self.user = User.objects.create_superuser('testuser', 'testuser@email.com', 'super_secret_pass')
+        self.user.save()
+
+    def test_logout(self):
+        request = self.factory.post('login/', {'username': 'testuser', 'password': 'super_secret_pass'})
+        request.user = self.user
+
+        self.middleware.process_request(request)
+        request.session.save()
+
+        log_in(request)
+
+        log_out(request)
+
+        self.assertFalse(request.user.is_authenticated)
