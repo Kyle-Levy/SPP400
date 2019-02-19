99798ef91693e44ce304c28a3ac05f556c92b290
Kyle <kdorcey5103@gmail.com>
2019-02-19 11:45:11 -0600

    added fields to explain why a patient is flagged. Also set database default values

--- patients/models.py
+++ patients/models.py
@@ -12,9 +12,10 @@ class Patients(models.Model):
     bday = models.DateField(auto_now=False, auto_now_add=False)
     doc_notes = models.TextField()
     flagged = models.BooleanField(default=False)
-    flagged_reason = models.TextField()
+    flagged_reason = models.TextField(default="")
     today_flag = models.BooleanField(default=False)
-    today_flag_end = models.DateTimeField()
+    today_flag_end = models.DateTimeField(default=datetime.now())
+    today_flag_reason = models.TextField(default="")
 
 
     def toggle_today_flag(self):
@@ -26,6 +27,7 @@ class Patients(models.Model):
             self.today_flag = False
             self.today_flag_end(blank=True)
             return False
+
     #returns true if flag is still valid
     def check_today_flag(self):
         if self.today_flag is True and timezone.now() <= self.today_flag_end is True:
