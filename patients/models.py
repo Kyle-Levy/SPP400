00b0808978a4ca7502a0a48a4ca53d1a24c519eb
Kyle <kdorcey5103@gmail.com>
2019-02-19 11:41:49 -0600

    Added functions for flagging/removing a patients "today_flag"
    the today flag is a way for a doctor/nurse to mark that a patient will be visiting for a procedure today

--- patients/models.py
+++ patients/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from datetime import datetime
+from datetime import timedelta
+from django.utils import timezone
 
 # Create your models here.
 
@@ -9,4 +12,28 @@ class Patients(models.Model):
     bday = models.DateField(auto_now=False, auto_now_add=False)
     doc_notes = models.TextField()
     flagged = models.BooleanField(default=False)
+    flagged_reason = models.TextField()
     today_flag = models.BooleanField(default=False)
+    today_flag_end = models.DateTimeField()
+
+
+    def toggle_today_flag(self):
+        if self.today_flag is False:
+            self.today_flag = True
+            self.today_flag_end = timezone.now() + timedelta(days=1)
+            return True
+        else:
+            self.today_flag = False
+            self.today_flag_end(blank=True)
+            return False
+    #returns true if flag is still valid
+    def check_today_flag(self):
+        if self.today_flag is True and timezone.now() <= self.today_flag_end is True:
+            return True
+        if self.today_flag is True and timezone.now() > self.today_flag_end is True:
+            self.toggle_today_flag()
+            return False
+        else:
+            return False
+
+
