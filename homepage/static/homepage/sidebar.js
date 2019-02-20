36a1c0f6291f228aaab8e1f7f57b265fc43b022b
kylelevy <kyle.robert.levy@gmail.com>
2019-02-19 22:33:50 -0600

    Homepage html and js to open and close sidebar

---
+++ homepage/static/homepage/sidebar.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+    $('#sidebar').toggleClass('active');
+
+    $('#sidebarCollapse').on('click', function() {
+        $('#sidebar').toggleClass('active');
+    });
+});
