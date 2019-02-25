178c231a4c259b8370c6ecbcd7ab27aef1555334
Kyle <kdorcey5103@gmail.com>
2019-02-24 19:37:24 -0600

    Created model for Procedures

--- procedures/models.py
+++ procedures/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
+from datetime import timedelta
+from django.utils import timezone
 
-# Create your models here.
+class Procedures(models.Model):
+    procedure_name = models.CharField(max_length=1000, default="")
+    procedure_info = models.CharField(max_length=1000, default="")
+
+    def __str__(self):
+        return self.procedure_name
