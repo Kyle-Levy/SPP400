d0077d8902855bc9ec52589346a02a4b557f64a7
kylelevy <kyle.robert.levy@gmail.com>
2019-02-24 20:15:56 -0600

    Front end set up for patient creation

---
+++ patients/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+
+
+class NewPatient(forms.Form):
+    first_name = forms.CharField(
+        label='First Name',
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'id': 'firstName',
+                'placeholder': "Patient's first name"
+            }
+        )
+    )
+
+    last_name = forms.CharField(
+        label='Last Name',
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'id': 'lastName',
+                'placeholder': "Patient's last name"
+            }
+        )
+    )
+
+    birth_date = forms.DateField(
+        label='Birthday',
+        required=False,
+        widget=forms.DateInput(
+            attrs={
+                'class': 'form-control',
+                'id': 'birthDate',
+                'placeholder': 'MM/DD/YYYY',
+            }
+        )
+    )
--- patients/views.py
+++ patients/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from patients.forms import NewPatient
 
 
 def index(request):
     if request.method == 'GET':
         return render(request, 'landing_page.html')
+
+
+def new_patient(request):
+    if request.method == 'GET':
+        return render(request, 'new_patient.html', {'form': NewPatient()})
