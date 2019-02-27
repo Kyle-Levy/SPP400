91b8273962d8b68ee6658cd3ad2c3e0f4303606f
kylelevy <kyle.robert.levy@gmail.com>
2019-02-27 16:05:33 -0600

    patients update tested for both post and get routes

--- patients/tests_views.py
+++ patients/tests_views.py
@@ -2,7 +2,7 @@ from django.test import RequestFactory, TestCase
 from django.contrib.auth.models import User
 from django.contrib.sessions.middleware import SessionMiddleware
 
-from .views import index, new_patient
+from .views import index, new_patient, profile, update, delete
 from .models import Patients
 
 
@@ -15,8 +15,16 @@ class TestCreatePatient(TestCase):
         self.user = User.objects.create_superuser('testuser', 'testuser@email.com', 'super_secret_pass')
         self.user.save()
 
-    def test_valid_patient(self):
+        # Creating patient for testing the ability to manipulate a patient
         request = self.factory.post('create/', {'first_name': 'John', 'last_name': 'Smith', 'birth_date': '1950-01-01'})
+        request.user = self.user
+        self.middleware.process_request(request)
+        request.session.save()
+        new_patient(request)
+        self.test_patient = Patients.objects.get(first_name='John', last_name='Smith', bday='1950-01-01')
+
+    def test_valid_patient(self):
+        request = self.factory.post('create/', {'first_name': 'Marie', 'last_name': 'Smith', 'birth_date': '1950-02-01'})
         self.middleware.process_request(request)
         request.session.save()
         request.user = self.user
@@ -27,74 +35,99 @@ class TestCreatePatient(TestCase):
 
         # If the patient does exist, the value of None will be overwritten
 
-        created_patient = Patients.objects.get(first_name='John', last_name='Smith', bday='1950-01-01')
+        created_patient = Patients.objects.get(first_name='Marie', last_name='Smith', bday='1950-02-01')
 
         self.assertIsNotNone(created_patient)
 
-def test_missing_first_name(self):
-    request = self.factory.post('create/', {'last_name': 'Smith', 'birth_date': '1950-01-01'})
-    self.middleware.process_request(request)
-    request.session.save()
-    request.user = self.user
+    def test_missing_first_name(self):
+        request = self.factory.post('create/', {'last_name': 'Smith', 'birth_date': '1950-02-01'})
+        self.middleware.process_request(request)
+        request.session.save()
+        request.user = self.user
 
-    response = new_patient(request)
+        response = new_patient(request)
 
-    self.assertTrue(response.status_code, 401)
+        self.assertTrue(response.status_code, 401)
+
+    def test_missing_last_name(self):
+        request = self.factory.post('create/', {'first_name': 'Marie', 'birth_date': '1950-02-01'})
+        self.middleware.process_request(request)
+        request.session.save()
+        request.user = self.user
+
+        response = new_patient(request)
+
+        self.assertTrue(response.status_code, 401)
+
+    def test_missing_bday(self):
+        request = self.factory.post('create/', {'first_name': 'Marie', 'last_name': 'Smith'})
+        self.middleware.process_request(request)
+        request.session.save()
+        request.user = self.user
+
+        response = new_patient(request)
+
+        self.assertTrue(response.status_code, 401)
+
+    def test_get_patients(self):
+        request = self.factory.get('/patients/')
+
+        self.middleware.process_request(request)
+        request.session.save()
+        request.user = self.user
+
+        response = index(request)
+
+        self.assertTrue(response.status_code, 200)
+
+    def test_get_new_patients(self):
+        request = self.factory.get('/patients/create/')
+
+        self.middleware.process_request(request)
+        request.session.save()
+        request.user = self.user
+
+        response = new_patient(request)
+
+        self.assertTrue(response.status_code, 200)
+
+    def test_get_profile_valid_id(self):
+        request = self.factory.get('/patients/profile/?id=' + str(self.test_patient.id))
+        request.user = self.user
+        self.middleware.process_request(request)
+        request.session.save()
+
+        response = profile(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_view_procedure_invalid_id(self):
+        request = self.factory.get('/patients/profile/?id=' + str(999999999999))
+        request.user = self.user
+        self.middleware.process_request(request)
+        request.session.save()
+
+        response = profile(request)
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_view_procedure_valid_id(self):
+        request = self.factory.post('/patients/profile/?id=' + str(self.test_patient.id))
+        request.user = self.user
+        self.middleware.process_request(request)
+        request.session.save()
+
+        response = profile(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_view_procedure_invalid_id(self):
+        request = self.factory.post('/patients/profile/?id=' + str(99999))
+        request.user = self.user
+        self.middleware.process_request(request)
+        request.session.save()
+
+        response = profile(request)
+        self.assertEqual(response.status_code, 302)
 
 
-def test_missing_last_name(self):
-    request = self.factory.post('create/', {'first_name': 'John', 'birth_date': '1950-01-01'})
-    self.middleware.process_request(request)
-    request.session.save()
-    request.user = self.user
-
-    response = new_patient(request)
-
-    self.assertTrue(response.status_code, 401)
-
-
-def test_missing_bday(self):
-    request = self.factory.post('create/', {'first_name': 'John', 'last_name': 'Smith'})
-    self.middleware.process_request(request)
-    request.session.save()
-    request.user = self.user
-
-    response = new_patient(request)
-
-    self.assertTrue(response.status_code, 401)
-
-
-def test_get_patients(self):
-    request = self.factory.get('/patients/')
-
-    self.middleware.process_request(request)
-    request.session.save()
-    request.user = self.user
-
-    response = index(request)
-
-    self.assertTrue(response.status_code, 200)
-
-
-def test_get_new_patients(self):
-    request = self.factory.get('/patients/create/')
-
-    self.middleware.process_request(request)
-    request.session.save()
-    request.user = self.user
-
-    response = new_patient(request)
-
-    self.assertTrue(response.status_code, 200)
-
-
-class TestUpdatePatient(TestCase):
-    # create setUp so every test can have access to the requestFactory
-    def setUp(self):
-        self.factory = RequestFactory()
-        self.middleware = SessionMiddleware()
-        self.user = User.objects.create_superuser('testuser', 'testuser@email.com', 'super_secret_pass')
-        self.user.save()
 
 '''
 #If the patient does exist, the value of None will be overwritten.
