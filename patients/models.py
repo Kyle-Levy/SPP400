69bbf6cc0f17ece55970d80a8c5eb10301a20690
Evan Meyer <evanmeyer07@gmail.com>
2019-03-01 13:11:08 -0600

    Adding foreign key to patients Model so that procedure can be assigned.

--- patients/models.py
+++ patients/models.py
@@ -17,6 +17,8 @@ class Patients(models.Model):
     today_flag_end = models.DateTimeField(default=timezone.now)
     today_flag_reason = models.CharField(max_length = 1000, default="")
 
+    procedure_step = models.ForeignKey('procedures.Procedure', on_delete=models.CASCADE,)
+
 
     @classmethod
     def create_patient(cls, first_name, last_name, birth_date):
