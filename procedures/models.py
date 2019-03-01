e8a41c7e005a2add7081fb45e8d5819e4dee05c3
Evan Meyer <evanmeyer07@gmail.com>
2019-03-01 14:02:00 -0600

    Updated procedures to prepare for Roadmap application

--- procedures/models.py
+++ procedures/models.py
@@ -7,6 +7,9 @@ from django.utils import timezone
 class Procedure(models.Model):
     procedure_name = models.CharField(max_length=1000, default="")
     procedure_info = models.CharField(max_length=1000, default="")
+    procedure_flag = models.BooleanField
+    # Roadmap foreign key will be made. Step number will be set inside Roadmap.
+
 
     def __str__(self):
         return self.procedure_name
