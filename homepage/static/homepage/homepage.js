9d641c16e5ecf22779e2f054eb284d876438ea1a
kylelevy <kyle.robert.levy@gmail.com>
2019-03-06 20:51:52 -0600

    Added 'Sort' button to the homepage to sort the patients displayed by Last name

--- homepage/static/homepage/homepage.js
+++ homepage/static/homepage/homepage.js
@@ -5,4 +5,34 @@ $(document).ready(function () {
 
     $("#home > a:nth-child(1)").attr("href", "#");
 
+    var backwards = false;
+
+    $("#sortAll").click(function () {
+        var sort_by_last_name = function (a, b) {
+            return a.innerHTML.toLowerCase().localeCompare(b.innerHTML.toLowerCase());
+        }
+
+        console.log(backwards);
+
+        var list = $("a > div > h5").get();
+
+        list = list.sort(sort_by_last_name);
+
+
+        if (!backwards) {
+            for (var i = 0; i < list.length; i++) {
+                //list-group.append <a> of list[i]
+                list[i].parentNode.parentNode.parentNode.append(list[i].parentNode.parentNode);
+            }
+        }
+        else if(backwards){
+            for (var i = 0; i < list.length; i++) {
+                //list-group.append <a> of list[i]
+                list[i].parentNode.parentNode.parentNode.prepend(list[i].parentNode.parentNode);
+            }
+        }
+        backwards = !backwards;
+
+    });
+
 });
