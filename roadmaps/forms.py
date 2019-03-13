475790e746a44ba7bc9641981a5e87f6d326f7bd
kylelevy <kyle.robert.levy@gmail.com>
2019-03-13 17:01:21 -0500

    Front end ready for javascript

--- roadmaps/forms.py
+++ roadmaps/forms.py
@@ -1,8 +1,20 @@
-from django.forms import ModelForm
+from django import forms
 from roadmaps.models import Roadmap
 
 
-class RoadmapForm(ModelForm):
-    class Meta:
-        model = Roadmap
-        fields = ['roadmap_name', 'procedures', 'phases']
+class RoadmapForm(forms.Form):
+    roadmap_name = forms.CharField(
+        label='Roadmap Name',
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'id': 'roadmapName',
+                'placeholder': "Enter Roadmap Name"
+            }
+        )
+    )
+    procedure_phase_pairs = forms.MultipleChoiceField(required=True, widget=forms.SelectMultiple(attrs={
+        'id': 'procedurePairs'
+    }))
