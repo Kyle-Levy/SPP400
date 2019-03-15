cbf93ed43a3de95d66562717b169cb252ce797ba
Evan Meyer <evanmeyer07@gmail.com>
2019-03-14 19:51:17 -0500

    Updated creation test for new phases input

--- roadmaps/tests.py
+++ roadmaps/tests.py
@@ -4,7 +4,7 @@ from roadmaps.models import Roadmap,Procedure,RoadmapProcedureLink
 
 class TestRoadmaps(TestCase):
 
-    def create_RoadmapProcedureLink(self, roadmap_name='TAVR', procedure_name='blood work', procedure_info='extract blood', phases=('ONE', '1')):
+    def create_RoadmapProcedureLink(self, roadmap_name='TAVR', procedure_name='blood work', procedure_info='extract blood', phases=1):
         tRoadmap = Roadmap.objects.create(roadmap_name=roadmap_name)
         tProcedure = Procedure.objects.create(procedure_name=procedure_name, procedure_info=procedure_info)
         # replace this with a function for link creation.
