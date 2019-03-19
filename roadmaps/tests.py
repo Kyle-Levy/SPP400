831709352fdfa6a92768a2933dd210e713a07866
Kyle <kdorcey5103@gmail.com>
2019-03-19 17:17:10 -0500

    added passing test for get_procedures_From_roadmap

--- roadmaps/tests.py
+++ roadmaps/tests.py
@@ -1,12 +1,23 @@
 from django.test import TestCase
-from roadmaps.models import Roadmap,Procedure,RoadmapProcedureLink
+from procedures.models import Procedure
+from roadmaps.models import Roadmap,RoadmapProcedureLink
 
 
 class TestRoadmaps(TestCase):
 
     def create_RoadmapProcedureLink(self, roadmap_name='TAVR', procedure_name='blood work', procedure_info='extract blood', phases=1):
         tRoadmap = Roadmap.objects.create(roadmap_name=roadmap_name)
-        tProcedure = Procedure.objects.create(procedure_name=procedure_name, procedure_info=procedure_info)
-        # replace this with a function for link creation.
-        tRoadmapProcedureLink = RoadmapProcedureLink.link_procedure_to_roadmap(phases, tProcedure, tRoadmap)
+        tRoadmap.save()
+        tProcedure = Procedure.objects.create(procedure_name=procedure_name)
+        tProcedure.save()
+
+        tRoadmapProcedureLink = RoadmapProcedureLink.link_procedure_to_roadmap(tProcedure,tRoadmap,1)
+
         return tRoadmap, tProcedure, tRoadmapProcedureLink
+
+    def test_get_procedures_from_roadmap(self):
+        testRoadmap, testProc, testRoadmap = self.create_RoadmapProcedureLink()
+
+        test = RoadmapProcedureLink.get_procedures_from_roadmap(testRoadmap)
+        want = [(testProc,1)]
+        self.assertEqual(test, want)
