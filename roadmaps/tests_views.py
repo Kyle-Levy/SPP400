631811a9a3d5f78a3cf4a4bd8b9d4d8fcd7fdcda
kylelevy <kyle.robert.levy@gmail.com>
2019-03-25 14:57:26 -0500

    GET route of view_roadmap is tested

--- roadmaps/tests_views.py
+++ roadmaps/tests_views.py
@@ -1,8 +1,8 @@
 from django.test import RequestFactory, TestCase
 from django.contrib.sessions.middleware import SessionMiddleware
 from django.contrib.auth.models import User
-from procedures.views import index, new_procedure, view_procedure, update_procedure, delete_this_procedure
-from roadmaps.views import roadmaps_index, create_roadmap
+from procedures.views import new_procedure
+from roadmaps.views import roadmaps_index, create_roadmap, view_roadmap
 from .models import Procedure, Roadmap, RoadmapProcedureLink
 
 
@@ -44,6 +44,14 @@ class TestProcedures(TestCase):
         self.test_object_three = Procedure.objects.get(procedure_name='Object Three',
                                                        procedure_info='These are test notes')
 
+        request = self.factory.post('/roadmaps/create/', {'roadmap_name': 'Test Roadmap'})
+        request.user = self.user
+        self.middleware.process_request(request)
+        request.session.save()
+
+        create_roadmap(request)
+        self.test_roadmap = Roadmap.objects.get(roadmap_name='Test Roadmap')
+
     def test_get_roadmaps(self):
         request = self.factory.get('/roadmaps/')
         request.user = self.user
@@ -72,7 +80,7 @@ class TestProcedures(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_create_roadmap_valid(self):
-        request = self.factory.post('/roadmaps/create/', {'roadmap_name': 'Test Roadmap'})
+        request = self.factory.post('/roadmaps/create/', {'roadmap_name': 'Created Roadmap'})
         request.user = self.user
         self.middleware.process_request(request)
         request.session.save()
@@ -80,11 +88,11 @@ class TestProcedures(TestCase):
         response = create_roadmap(request)
         self.assertEqual(response.status_code, 302)
 
-        test_roadmap = Roadmap.objects.get(roadmap_name='Test Roadmap')
+        test_roadmap = Roadmap.objects.get(roadmap_name='Created Roadmap')
         self.assertIsNotNone(test_roadmap)
 
     def test_create_roadmap_invalid(self):
-        request = self.factory.post('/roadmaps/create/', { })
+        request = self.factory.post('/roadmaps/create/', {})
         request.user = self.user
         self.middleware.process_request(request)
         request.session.save()
@@ -92,4 +100,22 @@ class TestProcedures(TestCase):
         response = create_roadmap(request)
         self.assertEqual(response.status_code, 401)
 
-        
+    def test_get_view_roadmap_valid(self):
+        request = self.factory.get('/roadmaps/view_roadmap/?id=' + str(self.test_roadmap.id))
+        request.user = self.user
+        self.middleware.process_request(request)
+        request.session.save()
+
+        response = view_roadmap(request)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_view_roadmap_invalid(self):
+        request = self.factory.get('/roadmaps/view_roadmap/?id=' + str(self.test_roadmap.id+99999))
+        request.user = self.user
+        self.middleware.process_request(request)
+        request.session.save()
+
+        response = view_roadmap(request)
+
+        self.assertEqual(response.status_code, 302)
