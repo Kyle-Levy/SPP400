ccbd0fce50715bd0b1291e6ad0afbdeca2ebfb06
Evan Meyer <evanmeyer07@gmail.com>
2019-03-31 17:35:13 -0500

    Fixed date entry error.

--- patients/tests_forms.py
+++ patients/tests_forms.py
@@ -6,31 +6,31 @@ class TestPatientForm(TestCase):
 
     def test_valid_form(self):
         form_data = {'first_name': 'John', 'last_name': 'Smith', 'record_number': 'a', 'birth_date': '01/01/1950',
-                     'referring_physician': 'Dr. Who', 'date_of_referral': '01/01/2019'}
+                     'referring_physician': 'Dr. Who', 'date_of_referral': '2019-01-01'}
         form = NewPatient(data=form_data)
         self.assertTrue(form.is_valid())
 
     def test_invalid_date(self):
         form_data = {'first_name': 'John', 'last_name': 'Smith', 'record_number': 'a', 'birth_date': '418',
-                     'referring_physician': 'Dr. Who', 'date_of_referral': '01/01/2019'}
+                     'referring_physician': 'Dr. Who', 'date_of_referral': '2019-01-01'}
         form = NewPatient(data=form_data)
         self.assertFalse(form.is_valid())
 
     def test_missing_date(self):
         form_data = {'first_name': 'John', 'last_name': 'Smith', 'record_number': 'a', 'referring_physician': 'Dr. Who',
-                     'date_of_referral': '01/01/2019'}
+                     'date_of_referral': '2019-01-01'}
         form = NewPatient(data=form_data)
         self.assertFalse(form.is_valid())
 
     def test_missing_first_name(self):
         form_data = {'last_name': 'Smith', 'birth_date': '418', 'record_number': 'a', 'referring_physician': 'Dr. Who',
-                     'date_of_referral': '01/01/2019'}
+                     'date_of_referral': '2019-01-01'}
         form = NewPatient(data=form_data)
         self.assertFalse(form.is_valid())
 
     def test_missing_last_name(self):
         form_data = {'first_name': 'John', 'birth_date': '418', 'record_number': 'a', 'referring_physician': 'Dr. Who',
-                     'date_of_referral': '01/01/2019'}
+                     'date_of_referral': '2019-01-01'}
         form = NewPatient(data=form_data)
         self.assertFalse(form.is_valid())
 
--- patients/tests_models.py
+++ patients/tests_models.py
@@ -5,7 +5,7 @@ from patients.models import Patients
 class TestPatientsModel(TestCase):
 
     def create_patient(self, first_name='Steve', last_name='Stevens', bday='2000-01-01', referring_physician='Dr. Who',
-                       date_of_referral='01/01/2019'):
+                       date_of_referral='2019-01-01'):
         return Patients.objects.create(first_name=first_name, last_name=last_name, bday=bday,
                                        referring_physician=referring_physician, date_of_referral=date_of_referral)
 
