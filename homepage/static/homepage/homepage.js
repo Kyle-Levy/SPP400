a8af7147fd535a9e3aea897db26fae025eb2df77
kylelevy <kyle.robert.levy@gmail.com>
2019-04-02 20:25:52 -0500

    Fixed filtering to work for any column on the homepage

--- homepage/static/homepage/homepage.js
+++ homepage/static/homepage/homepage.js
@@ -11,51 +11,65 @@ $(document).ready(function () {
     //Create function show_checked that grabs whether the checkboxes are checked or not, then only shows the checked catagories.
     //Might give a couple inputs later that allow it to be used on different checklists & different patient panels
     let show_checked = function () {
-        let checkList = $("#patients-col > label > input").get();
 
-        //Default values for checks
-        let referred = "none";
-        let inprogress = "none";
-        let ready = "none";
-        let done = "none";
+        //This should eventually be all columns
+        let manipulated_columns = ['patients-col'];
 
-        //If checked, the elements should be displayed so display attribute is empty
-        if (checkList[0].checked) {
-            referred = "";
+        //On document ready, this would be an instance of, hence it should sort all columns, else, it should sort only the grandparent of the checkbox clicked's column(i.e patients-col, alerts-col)
+        if (!(this instanceof Window)) {
+            manipulated_columns = [$(this.parentNode.parentNode).attr('id')];
         }
 
-        if (checkList[1].checked) {
-            inprogress = "";
-        }
+        console.log(manipulated_columns);
 
-        if (checkList[2].checked) {
-            ready = "";
-        }
+        for (var j = 0; j < manipulated_columns.length; j++) {
+            let checkList = $("#" + manipulated_columns[j] + " > label > input").get();
 
-        if (checkList[3].checked) {
-            done = "";
-        }
+            console.log(checkList);
 
-        //Get all of the 'statuses' of the patients in the list group
-        let patient_statuses = $("a > small").get();
+            //Default values for checks
+            let referred = "none";
+            let inprogress = "none";
+            let ready = "none";
+            let done = "none";
 
-
-        //For all of the elements, set the display attr
-        for (let i = 0; i < patient_statuses.length; i++) {
-            if (patient_statuses[i].innerHTML === "Referred") {
-                patient_statuses[i].parentNode.style.display = referred;
+            //If checked, the elements should be displayed so display attribute is empty
+            if (checkList[0].checked) {
+                referred = "";
             }
-            if (patient_statuses[i].innerHTML === "In-Progress") {
-                patient_statuses[i].parentNode.style.display = inprogress;
+
+            if (checkList[1].checked) {
+                inprogress = "";
             }
-            if (patient_statuses[i].innerHTML === "Ready") {
-                patient_statuses[i].parentNode.style.display = ready;
+
+            if (checkList[2].checked) {
+                ready = "";
             }
-            if (patient_statuses[i].innerHTML === "Done") {
-                patient_statuses[i].parentNode.style.display = done;
+
+            if (checkList[3].checked) {
+                done = "";
+            }
+
+            //Get all of the 'statuses' of the patients in the list group
+            let patient_statuses = $("#" + manipulated_columns[j] + " > div > a > small").get();
+
+
+            //For all of the elements, set the display attr
+            for (let i = 0; i < patient_statuses.length; i++) {
+                if (patient_statuses[i].innerHTML === "Referred") {
+                    patient_statuses[i].parentNode.style.display = referred;
+                }
+                if (patient_statuses[i].innerHTML === "In-Progress") {
+                    patient_statuses[i].parentNode.style.display = inprogress;
+                }
+                if (patient_statuses[i].innerHTML === "Ready") {
+                    patient_statuses[i].parentNode.style.display = ready;
+                }
+                if (patient_statuses[i].innerHTML === "Done") {
+                    patient_statuses[i].parentNode.style.display = done;
+                }
             }
         }
-
 
     };
 
@@ -65,7 +79,7 @@ $(document).ready(function () {
     //Apply the function as a listener to the checkboxes themselves
     $("#patients-col > label > input").change(show_checked);
 
-    let sort_list_group = function (event) {
+    let sort_list_group = function () {
 
         //Comparator function between a's text and b's text
         let sort_by_last_name = function (a, b) {
@@ -76,13 +90,13 @@ $(document).ready(function () {
             return a.innerHTML.substring(a.innerHTML.indexOf(',') + 1).localeCompare(b.innerHTML.substring(b.innerHTML.indexOf(',') + 1));
         };
 
-        
 
+        //Lists of patients names
         let list = $("a > div > h5").get();
 
         if (event.data.sort_method === "last") {
             list = list.sort(sort_by_last_name);
-        }else if(event.data.sort_method === "first"){
+        } else if (event.data.sort_method === "first") {
             list = list.sort(sort_by_first_name)
         }
 
@@ -104,7 +118,6 @@ $(document).ready(function () {
     $("#sortByFirst").click({sort_method: "first"}, sort_list_group);
     //Sort the patients by last name
     $("#sortByLast").click({sort_method: "last"}, sort_list_group);
-
 
 
 });
