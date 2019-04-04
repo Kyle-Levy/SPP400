b0fd0489ff2dba00e899c5325945e76c8d0d62a6
Kyle <kdorcey5103@gmail.com>
2019-04-04 16:45:23 -0500

    it wasn't a fix.

--- SPP400/settings.py
+++ SPP400/settings.py
@@ -145,6 +145,7 @@ SESSION_COOKIE_AGE = 15 * 60
 SESSION_SAVE_EVERY_REQUEST = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'SEP400test@gmail.com'
