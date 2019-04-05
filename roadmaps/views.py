1bc64b218c1b5583f69af4be34cd0bc1c12ac0e1
Kyle Levy <kyle-levy@uiowa.edu>
2019-04-05 14:32:27 -0500

    Fixed the routing of roadmaps, texting is next

--- roadmaps/views.py
+++ roadmaps/views.py
@@ -39,28 +39,9 @@ def create_roadmap(request):
                        'breadcrumbs': breadcrumbs})
 
 
+# Todo this method used to set the session
 @login_required
 def view_roadmap(request):
-    # User wants to update the roadmap currently being viewed
-    if request.method == 'POST':
-        try:
-            # Get the roadmap and place its id into the session
-            roadmap = Roadmap.objects.get(id=request.GET.get('id'))
-            roadmap_pairs = RoadmapProcedureLink.get_procedures_from_roadmap(roadmap)
-            request.session['roadmap_id'] = request.GET.get('id')
-            breadcrumbs = [('/roadmaps/', 'Roadmaps'),
-                           ('/roadmaps/view_roadmap/?id=' + request.GET.get('id'), roadmap.roadmap_name),
-                           ('#', 'Modifying: ' + roadmap.roadmap_name)]
-            return render(request, 'modify_roadmap.html',
-                          {'form': RoadmapProcedureLinkForm(), 'roadmap_pairs': roadmap_pairs, 'roadmap': roadmap,
-                           'title': 'Modifying: ' + roadmap.roadmap_name, 'breadcrumbs': breadcrumbs,
-                           'update_form': RoadmapForm(initial={'time': roadmap.est_days_to_complete,
-                                                               'time_frame': 'days',
-                                                               'roadmap_name': roadmap.roadmap_name}),
-                           'verification_form': VerifyActionForm()})
-        except Roadmap.DoesNotExist:
-            return redirect('/roadmaps/')
-
     if request.method == 'GET':
         try:
             roadmap = Roadmap.objects.get(id=request.GET.get('id'))
@@ -77,113 +58,109 @@ def view_roadmap(request):
 
 
 @login_required
+def modify_roadmap(request):
+    # User wants to update the roadmap currently being viewed
+    if request.method == 'GET':
+        try:
+            # Get the roadmap and place its id into the session
+            roadmap = Roadmap.objects.get(id=request.GET.get('id'))
+            roadmap_pairs = RoadmapProcedureLink.get_procedures_from_roadmap(roadmap)
+
+            breadcrumbs = [('/roadmaps/', 'Roadmaps'),
+                           ('/roadmaps/view_roadmap/?id=' + request.GET.get('id'), roadmap.roadmap_name),
+                           ('#', 'Modifying: ' + roadmap.roadmap_name)]
+
+            roadmap_initial_details = {'time': roadmap.est_days_to_complete,
+                                       'time_frame': 'days',
+                                       'roadmap_name': roadmap.roadmap_name}
+
+            return render(request, 'modify_roadmap.html',
+                          {'form': RoadmapProcedureLinkForm(), 'roadmap_pairs': roadmap_pairs, 'roadmap': roadmap,
+                           'title': 'Modifying: ' + roadmap.roadmap_name, 'breadcrumbs': breadcrumbs,
+                           'update_form': RoadmapForm(initial=roadmap_initial_details),
+                           'verification_form': VerifyActionForm()})
+
+        except Roadmap.DoesNotExist:
+            # TODO ERROR MESSAGE "Roadmap doesn't exist"
+            return redirect('/roadmaps/')
+
+
+# TODO THIS METHOD USED TO USE THE SESSION
+@login_required
 def add_to_roadmap(request):
     if request.method == 'POST':
+        roadmap_id = request.GET.get('id')
         form = RoadmapProcedureLinkForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            roadmap_id = request.session['roadmap_id']
             for procedure_item in cd['procedure']:
                 # If the item doesn't exist, add it
                 if not RoadmapProcedureLink.objects.filter(roadmap=roadmap_id, procedure=procedure_item.id,
                                                            phase=cd['phase']):
                     RoadmapProcedureLink.link_procedure_to_roadmap(procedure_item.id, roadmap_id, cd['phase'])
-            try:
-                roadmap = Roadmap.objects.get(id=roadmap_id)
-                roadmap_pairs = RoadmapProcedureLink.get_procedures_from_roadmap(roadmap)
-                breadcrumbs = [('/roadmaps/', 'Roadmaps'),
-                               ('/roadmaps/view_roadmap/?id=' + roadmap_id, roadmap.roadmap_name),
-                               ('#', 'Modifying: ' + roadmap.roadmap_name)]
-                return render(request, 'modify_roadmap.html',
-                              {'form': RoadmapProcedureLinkForm(), 'roadmap_pairs': roadmap_pairs, 'roadmap': roadmap,
-                               'title': 'Modifying: ' + roadmap.roadmap_name, 'breadcrumbs': breadcrumbs,
-                               'update_form': RoadmapForm(initial={'time': roadmap.est_days_to_complete,
-                                                                   'time_frame': 'days',
-                                                                   'roadmap_name': roadmap.roadmap_name}),
-                               'verification_form': VerifyActionForm()})
-            except Roadmap.DoesNotExist:
-                return redirect('/roadmaps')
+            return redirect('/roadmaps/view_roadmap/modify/?id=' + str(roadmap_id))
         else:
-            return redirect('/homepage/')
+            #TODO ERROR MESSAGE Invalid Form Submitted
+            return redirect('/roadmaps/view_roadmap/modify/?id=' + str(roadmap_id))
 
+
+# TODO THIS METHOD USED TO USE THE SESSION
 
 @login_required
 def remove_selected_pairs(request):
     if request.method == 'POST':
         checked_boxes = request.POST.getlist('selection[]')
-        roadmap_id = request.session['roadmap_id']
+        roadmap_id = request.GET.get('id')
 
         for pair in checked_boxes:
             cleaned_pair = tuple(pair.split(','))
             RoadmapProcedureLink.remove_pair_from_roadmap(roadmap_id, cleaned_pair[0],
                                                           cleaned_pair[1])
-        try:
-            roadmap = Roadmap.objects.get(id=roadmap_id)
-            roadmap_pairs = RoadmapProcedureLink.get_procedures_from_roadmap(roadmap)
-            breadcrumbs = [('/roadmaps/', 'Roadmaps'),
-                           ('/roadmaps/view_roadmap/?id=' + roadmap_id, roadmap.roadmap_name),
-                           ('#', 'Modifying: ' + roadmap.roadmap_name)]
-            return render(request, 'modify_roadmap.html',
-                          {'form': RoadmapProcedureLinkForm(), 'roadmap_pairs': roadmap_pairs, 'roadmap': roadmap,
-                           'title': 'Modifying: ' + roadmap.roadmap_name,
-                           'update_form': RoadmapForm(initial={'time': roadmap.est_days_to_complete,
-                                                               'time_frame': 'days',
-                                                               'roadmap_name': roadmap.roadmap_name}),
-                           'verification_form': VerifyActionForm()})
-        except Roadmap.DoesNotExist:
-            return redirect('/roadmaps')
 
-        current_username = request.user.username
-        user = authenticate
+        return redirect('/roadmaps/view_roadmap/modify/?id=' + str(roadmap_id))
 
+
+
+
+# TODO THIS METHOD USED TO USE THE SESSION
 
 @login_required
 def delete_roadmap(request):
     if request.method == 'POST':
         form = VerifyActionForm(request.POST)
         try:
-            roadmap_id = request.session['roadmap_id']
+            roadmap_id = request.GET.get('id')
+            print(roadmap_id)
             roadmap = Roadmap.objects.get(id=roadmap_id)
-            roadmap_pairs = RoadmapProcedureLink.get_procedures_from_roadmap(roadmap)
-            breadcrumbs = [('/roadmaps/', 'Roadmaps'),
-                           ('/roadmaps/view_roadmap/?id=' + str(roadmap.id), roadmap.roadmap_name),
-                           ('#', 'Modifying: ' + roadmap.roadmap_name)]
             current_username = request.user.username
+
+            #Text exists in the password box
             if form.is_valid():
                 cd = form.cleaned_data
                 user = authenticate(username=current_username, password=cd['password'])
+                #Password matches
                 if user is not None:
-
                     RoadmapProcedureLink.remove_all_pairs_from_roadmap(roadmap_id)
                     roadmap.delete()
                     return redirect('/roadmaps/')
+                #Password doesn't match
                 else:
-                    return render(request, 'modify_roadmap.html',
-                                  {'form': RoadmapProcedureLinkForm(), 'roadmap_pairs': roadmap_pairs,
-                                   'roadmap': roadmap,
-                                   'title': 'Modifying: ' + roadmap.roadmap_name, 'breadcrumbs': breadcrumbs,
-                                   'update_form': RoadmapForm(initial={'time': roadmap.est_days_to_complete,
-                                                                       'time_frame': 'days',
-                                                                       'roadmap_name': roadmap.roadmap_name}),
-                                   'verification_form': VerifyActionForm(initial={'password': ""})})
+                    #TODO ERROR MESSAGE Wrong Password
+                    return redirect('/roadmaps/view_roadmap/modify/?id=' + str(roadmap_id))
             else:
-                # invalid form should render the modify_roadmap.html with all of its forms and info
-                return render(request, 'modify_roadmap.html',
-                              {'form': RoadmapProcedureLinkForm(), 'roadmap_pairs': roadmap_pairs,
-                               'roadmap': roadmap,
-                               'title': 'Modifying: ' + roadmap.roadmap_name, 'breadcrumbs': breadcrumbs,
-                               'update_form': RoadmapForm(initial={'time': roadmap.est_days_to_complete,
-                                                                   'time_frame': 'days',
-                                                                   'roadmap_name': roadmap.roadmap_name}),
-                               'verification_form': VerifyActionForm(initial={'password': ""}), 'failed_delete': True})
+
+                #TODO Error message INVALID FORM
+                return redirect('/roadmaps/view_roadmap/modify/?id=' + str(roadmap_id))
         except Roadmap.DoesNotExist:
-            return redirect('/homepage')
+            #TODO The roadmap you tried to reach doesn't exist
+            return redirect('/roadmaps/')
 
 
+# TODO THIS METHOD USED TO USE THE SESSION
 @login_required
 def update_roadmap(request):
     if request.method == 'POST':
-        roadmap_id = request.session['roadmap_id']
+        roadmap_id = request.GET.get('id')
         form = RoadmapForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
@@ -192,17 +169,10 @@ def update_roadmap(request):
                 roadmap.roadmap_name = cd['roadmap_name']
                 roadmap.add_time_estimate(cd['time'], str(request.POST.get('time_frame')))
                 roadmap.save()
-                roadmap_pairs = RoadmapProcedureLink.get_procedures_from_roadmap(roadmap)
-                breadcrumbs = [('/roadmaps/', 'Roadmaps'),
-                               ('/roadmaps/view_roadmap/?id=' + str(roadmap.id), roadmap.roadmap_name),
-                               ('#', 'Modifying: ' + roadmap.roadmap_name)]
-                return render(request, 'modify_roadmap.html',
-                              {'form': RoadmapProcedureLinkForm(), 'roadmap_pairs': roadmap_pairs, 'roadmap': roadmap,
-                               'title': 'Modifying: ' + roadmap.roadmap_name, 'breadcrumbs': breadcrumbs,
-                               'update_form': RoadmapForm(initial={'time': roadmap.est_days_to_complete,
-                                                                   'time_frame': 'days',
-                                                                   'roadmap_name': roadmap.roadmap_name}),
-                               'verification_form': VerifyActionForm(initial={'password': ""})})
+                return redirect('/roadmaps/view_roadmap/modify/?id=' + str(roadmap_id))
             except Roadmap.DoesNotExist:
+                # TODO The roadmap you tried to reach doesn't exist
                 return redirect('/homepage')
-        return redirect('/roadmaps')
+        else:
+            #TODO Error Message INVALID FORM
+            return redirect('/roadmaps/view_roadmap/modify/?id=' + str(roadmap_id))
