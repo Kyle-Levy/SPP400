6bf0463e79f3b2850bdc6a319e0c59ccf438dfc0
Kyle <kdorcey5103@gmail.com>
2019-04-09 20:26:28 -0500

    updated analytics database columns, also added method for calculating/saving the % of behind procedures

--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from datetime import datetime
+from datetime import timedelta
+from assigned_procedures.models import AssignedProcedures
 
 # Create your models here.
+class Analytics(models.Model):
+    behind_procedure_perc = models.DecimalField(max_digits=5, decimal_places=2, default=000)
+
+    @staticmethod
+    def calculate_behind_procedure_prec():
+
+        totalProc = 0
+        for indivAssignedProc in AssignedProcedures.get_all_active_procedures():
+            totalProc += 1
+
+        behindProc = 0
+        for flaggedPatients in AssignedProcedures.update_and_return_all_patient_goal_flags():
+            if flaggedPatients.behind_flag is True:
+                behindProc += 1
+
+        percentBehind = (behindProc/totalProc) * 100
+
+        Analytics.behind_procedure_perc = percentBehind
+
+        return percentBehind
