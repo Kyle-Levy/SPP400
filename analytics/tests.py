cb8a3bef2c24fb0dbc9db10da27326f5c3780259
Kyle <kdorcey5103@gmail.com>
2019-04-09 20:58:48 -0500

    added passing test for return_all_behind_procedures

--- analytics/tests.py
+++ analytics/tests.py
@@ -27,3 +27,13 @@ class analytics_tests(TestCase):
 
         self.assertEqual(Analytics.calculate_behind_procedure_prec(), 50)
         self.assertEqual(Analytics.behind_procedure_perc, 50)
+
+    def test_return_all_behind_procedures(self):
+        tpatient, tprocedure, tassignedProc = self.test_create_test_inputs()
+        self.assertEqual(Analytics.return_all_behind_procedures(), [])
+
+        tprocedure2 = Procedure.objects.create(procedure_name= "TAVR Old", est_days_to_complete=-2)
+
+        solution = AssignedProcedures.assign_procedure_to_patient(2,tpatient, tprocedure2)
+
+        self.assertEqual(Analytics.return_all_behind_procedures(),[solution])
