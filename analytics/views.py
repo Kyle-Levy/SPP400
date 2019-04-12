96034fbd7a426c1dbd7527f557c6e7a358066842
GregMalicki <gregory-malicki@uiowa.edu>
2019-04-11 23:16:54 -0500

    reactivated analytics tab and added an empty index page

--- analytics/views.py
+++ analytics/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from homepage.forms import VerifyActionForm
+from django.contrib import messages
 
-# Create your views here.
+@login_required
+def index(request):
+    if request.method == 'GET':
+        breadcrumbs = [('/analytics/', 'Analytics')]
+        return render(request, 'analytics_main.html', {'title': 'Analytics', 'breadcrumbs': breadcrumbs})
