976a1c0bcda15e281a3f4e681fd246416c520c30
GregMalicki <gregory-malicki@uiowa.edu>
2019-04-14 00:23:07 -0500

    added an individual procedure statistics dropdown to analytics, changed format of display to 1 line

--- analytics/views.py
+++ analytics/views.py
@@ -2,9 +2,10 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from homepage.forms import VerifyActionForm
 from django.contrib import messages
+from assigned_procedures.models import AssignedProcedures, Procedure
 
 @login_required
 def index(request):
     if request.method == 'GET':
         breadcrumbs = [('/analytics/', 'Analytics')]
-        return render(request, 'analytics_main.html', {'title': 'Analytics', 'breadcrumbs': breadcrumbs})
+        return render(request, 'analytics_main.html', {'title': 'Analytics', 'breadcrumbs': breadcrumbs, 'procedures': Procedure.objects.all()})
