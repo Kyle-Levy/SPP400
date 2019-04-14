36961f1dccedf2ee5b949898c2e22adfd83f0937
GregMalicki <gregory-malicki@uiowa.edu>
2019-04-14 00:33:33 -0500

    added percent behind schedule header to analytics page

--- analytics/views.py
+++ analytics/views.py
@@ -2,10 +2,12 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from homepage.forms import VerifyActionForm
 from django.contrib import messages
-from assigned_procedures.models import AssignedProcedures, Procedure
+from assigned_procedures.models import AssignedProcedures
+from procedures.models import Procedure
+from analytics.models import Analytics
 
 @login_required
 def index(request):
     if request.method == 'GET':
         breadcrumbs = [('/analytics/', 'Analytics')]
-        return render(request, 'analytics_main.html', {'title': 'Analytics', 'breadcrumbs': breadcrumbs, 'procedures': Procedure.objects.all()})
+        return render(request, 'analytics_main.html', {'title': 'Analytics', 'breadcrumbs': breadcrumbs, 'procedures': Procedure.objects.all(), 'percent_behind': Analytics.calculate_behind_procedure_prec()})
