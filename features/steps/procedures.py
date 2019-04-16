c84eb9cf9f106b0a80312ebdbd3e359c9b288602
Evan Meyer <evanmeyer07@gmail.com>
2019-04-16 14:46:31 -0500

    Added given for successful login

---
+++ features/steps/procedures.py
@@ -0,0 +1,38 @@
+from behave import given, when, then
+from test.factories.accounts import UserFactory
+from django.contrib.auth import authenticate
+
+
+@given('a successful login')
+def step_impl(context):
+    u = UserFactory(username='foo', email='foo@example.com')
+    u.set_password('bar')
+
+    u.save()
+
+    br = context.browser
+    br.get(context.base_url + '/login/')
+
+    assert br.find_element_by_name('csrfmiddlewaretoken').is_enabled()
+
+    br.find_element_by_name('username').send_keys('foo')
+    br.find_element_by_name('password').send_keys('bar')
+    br.find_element_by_name('submit').click()
+
+    br = context.browser
+
+    assert br.find_element_by_name('csrfmiddlewaretoken').is_enabled()
+
+    user = authenticate(username='foo', password='bar')
+    user.profile.key = 94759374
+    user.save()
+
+    assert br.find_element_by_name('csrfmiddlewaretoken').is_enabled()
+    br.find_element_by_name('key').send_keys(94759374)
+    br.find_element_by_name('submit').click()
+
+    br = context.browser
+
+    assert br.current_url.endswith('/homepage/')
+
+@when('')
