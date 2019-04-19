17f6e3b9e293c781dd0b638d01602ab196f7716a
GregMalicki <gregory-malicki@uiowa.edu>
2019-04-18 20:54:12 -0500

    build default landing page for updating assigned procedure

--- assigned_procedures/views.py
+++ assigned_procedures/views.py
@@ -1,13 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-
 from homepage.forms import VerifyActionForm
-from patients.forms import NewPatient, SearchPatients, FlagForm
-from procedures.models import Procedure
-from roadmaps.forms import SelectFromRoadmap, RoadmapProcedureLinkForm
-from patients.models import Patients
+from assigned_procedures.forms import AssignedProcedureForm
+
 from assigned_procedures.models import AssignedProcedures
-from roadmaps.models import RoadmapProcedureLink
 from django.contrib import messages
 import collections
 # Create your views here.
@@ -15,9 +11,27 @@ import collections
 @login_required
 def update(request):
     if request.method == 'GET':
+        try:
+            # Get desired id from url
+            procedure = AssignedProcedures.objects.get(id=request.GET.get('id'))
+
+            # Build form
+
+            breadcrumbs = [('/procedures/', 'Procedures'),
+                           ('#', 'View: ' + procedure.procedure_name)]
+
+            return render(request, 'view_procedure.html',
+                          {'procedure': procedure, 'title': 'View: ' + procedure.procedure_name,
+                           'breadcrumbs': breadcrumbs})
+        except AssignedProcedures.DoesNotExist:
+            messages.warning(request, "The procedure you tried to reach doesn't exist!")
+            return redirect('/procedures/')
+
+
+
         breadcrumbs = [('#', 'Assigned')]
         return render(request, 'assigned_procedure.html',
-                      {'patients': None, 'form': SearchPatients(), 'filter': None,
-                       'title': 'Patients', 'breadcrumbs': breadcrumbs})
+                      {'patients': None, 'form': AssignedProcedureForm(),
+                       'title': 'Assigned', 'breadcrumbs': breadcrumbs})
     if request.method == 'POST':
         1+1
