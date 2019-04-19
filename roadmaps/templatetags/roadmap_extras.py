fff251b96933773de77f8c9721d2fe21c636b510
Gregory Malicki <gregory-malicki@uiowa.edu>
2019-04-19 12:31:23 -0500

    added template tag to get procedure id

--- roadmaps/templatetags/roadmap_extras.py
+++ roadmaps/templatetags/roadmap_extras.py
@@ -12,3 +12,12 @@ def modulo(num, val):
 @register.simple_tag()
 def average_completion_time(procedure_id):
     return AssignedProcedures.average_completion_time(procedure_id) + " days"
+
+
+@register.simple_tag()
+def assigned_procedure_id(patient, procedure, phase):
+    procedures = AssignedProcedures.objects.filter(patient=patient, procedure=procedure, phaseNumber=phase)
+    procedure_id = 0
+    for proc in procedures:
+        procedure_id = proc.id
+    return str(procedure_id)
