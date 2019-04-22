699f53d866c2510d5b49bc47fa4de56430536fb2
Alex <admichel415@yahoo.com>
2019-04-22 10:28:31 -0500

    added some TODOs and a sad path

--- features/steps/patients.py
+++ features/steps/patients.py
@@ -45,3 +45,22 @@ def step_impl(context):
     br = context.browser
     assert br.find_element_by_name('csrfmiddlewaretoken').is_enabled()
     assert br.current_url.endswith('/patients/')
+
+
+@when('I submit an invalid patients form')
+def step_impl(context):
+    br = context.browser
+    br.find_element_by_name('firstName').send_keys('Harry')
+    br.find_element_by_name('lastName').send_keys('Potter')
+    br.find_element_by_name('recordNumber').send_keys('Four')
+    br.find_element_by_name('birthDate').send_keys('Today')
+    br.find_element_by_name('referringPhysician').send_keys('Madam Pomfrey')
+    br.find_element_by_name('dateOfReferral').send_keys('Yesterday')
+    br.find_element_by_name('submit').click()
+
+
+@then('I will remain on the create a patient page')
+def step_impl(context):
+    br = context.browser
+    assert br.find_element_by_name('csrfmiddlewaretoken').is_enabled()
+    assert br.current_url.endswith('/patients/create/')
