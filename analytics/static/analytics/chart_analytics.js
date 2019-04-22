722b31717df751d36ccc5b904909ef304e37cd24
kylelevy <kyle.robert.levy@gmail.com>
2019-04-22 14:31:20 -0500

    JS is ready for bar graphing

--- analytics/static/analytics/chart_analytics.js
+++ analytics/static/analytics/chart_analytics.js
@@ -1,36 +1,48 @@
 $(document).ready(function () {
-    console.log("eeek");
 
     $("#analytic").addClass("active");
 
     $("#analytic > a:nth-child(1)").attr("href", "#");
 
 
-     var data = JSON.parse(received_data);
+     //var data = JSON.parse(received_data);
 
-     item = [{x:'Dec 2015', y:20}, {x:'Sep 2015', y:10}];
-    var ctx = document.getElementById('myChart').getContext('2d');
+     item = 0;
+var ctx = document.getElementById('myChart');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
         datasets: [{
-            label: '# of Votes',
-            data: item,
+            label: '# of Patients',
+            data: [1, 2, 4, 8, 16, 4, 8, 2, 16, 1, 4, 2],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(170, 152, 169, .4)',
+                'rgba(153, 102, 204, .4)',
+                'rgba(127, 255, 212, .4)',
+                'rgba(168, 195, 188, .4)',
+                'rgba(80, 200, 120, .4)',
+                'rgb(184,146,162, .4)',
+                'rgba(224, 17, 95, .4)',
+                'rgba(230, 226, 0, .4)',
+                'rgba(15, 82, 186, .4)',
+                'rgba(215, 59, 62, .4)',
+                'rgba(255, 165, 0, .4)',
+                'rgba(64, 224, 208, .4)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(170, 152, 169, 1)',
+                'rgba(153, 102, 204, 1)',
+                'rgba(127, 255, 212, 1)',
+                'rgba(168, 195, 188, 1)',
+                'rgba(80, 200, 120, 1)',
+                'rgb(184,146,162, 1)',
+                'rgba(224, 17, 95, 1)',
+                'rgba(230, 226, 0, 1)',
+                'rgba(15, 82, 186, 1)',
+                'rgba(215, 59, 62, 1)',
+                'rgba(255, 165, 0, 1)',
+                'rgba(64, 224, 208, 1)'
             ],
             borderWidth: 1
         }]
